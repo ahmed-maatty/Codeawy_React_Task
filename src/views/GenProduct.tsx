87299e9c0fec8aac23ee3e6e6d87910e813fbf93
ahmed nabil <am4201828@gmail.com>
2025-11-25 18:15:07 +0200

    createProduct

--- src/views/GenProduct.tsx
+++ src/views/GenProduct.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAppDispatch } from "../app/store";
+import { CreateProduct } from "../app/appSlice";
 
 type GenProductProps = {
   SetGenProduct: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 function GenProduct({ SetGenProduct }: GenProductProps) {
+  const [product, setProduct] = useState({});
+  const dispatch = useAppDispatch();
+  const new_product = (key: string, value: string) => {
+    setProduct((prev) => ({ ...prev, [`${key}`]: value }));
+  };
+  const submit_product = () => {
+    dispatch(CreateProduct(product));
+  };
   return (
     <div className="Gen_Product">
       <div className="Gen_Layout">
@@ -19,12 +29,20 @@ function GenProduct({ SetGenProduct }: GenProductProps) {
               type="text"
               placeholder="e.g wireless Noise Canceling"
               required
+              name="product_name"
+              onChange={(e) => new_product(e.target.name, e.target.value)}
             />
           </div>
           <div className="input_container two_inputs">
             <div>
               <label htmlFor="">Price</label>
-              <input type="text" placeholder="$ 99.99" />
+              <input
+                type="text"
+                placeholder="$ 99.99"
+                required
+                name="product_price"
+                onChange={(e) => new_product(e.target.name, e.target.value)}
+              />
             </div>
             <div>
               <label htmlFor="">Category</label>
@@ -44,9 +62,10 @@ function GenProduct({ SetGenProduct }: GenProductProps) {
           <div className="input_container">
             <label htmlFor="">Description (optional)</label>
             <textarea
-              name=""
+              name="product_desc"
               id=""
               placeholder="Descripe The Product"
+              onChange={(e) => new_product(e.target.name, e.target.value)}
             ></textarea>
           </div>
         </div>
@@ -54,7 +73,9 @@ function GenProduct({ SetGenProduct }: GenProductProps) {
           <button className="cancel_btn" onClick={() => SetGenProduct(false)}>
             cancel
           </button>
-          <button className="ok_btn">save product</button>
+          <button className="ok_btn" onClick={submit_product}>
+            save product
+          </button>
         </div>
       </div>
     </div>
